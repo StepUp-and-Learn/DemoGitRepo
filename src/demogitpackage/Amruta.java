e1e48c0b47fdb22f0351757e2426e450c123cd83
Omkar Kondekar <Omkar Kondekar@MSI>
2023-07-25 16:26:44 +0530

    hiiii

--- src/demogitpackage/Amruta.java
+++ src/demogitpackage/Amruta.java
@@ -6,6 +6,7 @@ public class Amruta {
 		// TODO Auto-generated method stub
       System.out.println("Hi..From Amruta");
       System.out.println("hi....I.Work in Git frmemwork");
+      System.out.println("Hello");
 	}
 
 }
