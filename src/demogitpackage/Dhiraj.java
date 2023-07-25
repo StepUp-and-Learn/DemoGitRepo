1076e2ea217694f4563a5a073f4de7738a3707db
Admin <Admin@DESKTOP-44KRVO6>
2023-07-25 21:31:58 +0530

    Added One class named Sudarshan

--- src/demogitpackage/Dhiraj.java
+++ src/demogitpackage/Dhiraj.java
@@ -2,7 +2,7 @@ package demogitpackage;
 
 public class Dhiraj {
 
-	
+	 
 	public void print()
 	{
 		System.out.println("Hi From Dhiraj");
---
+++ src/demogitpackage/Sudarshan.java
@@ -0,0 +1,10 @@
+package demogitpackage;
+
+public class Sudarshan {
+
+	public void print() {
+		
+		System.out.println("Hi From Sudarshan");
+		
+	}
+}
